a49a2787e40f63b22105ee312a2f8712358ae9b5
yanyanCHEN588 <58264804+yanyanCHEN588@users.noreply.github.com>
2022-04-25 21:43:18 +0800

    CurrentTimeMillisClock

--- app/src/main/java/com/example/basework_test/ViewActivity.java
+++ app/src/main/java/com/example/basework_test/ViewActivity.java
@@ -3,6 +3,7 @@ package com.example.basework_test;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -14,13 +15,24 @@ public class ViewActivity extends AppCompatActivity {
 //    Button btAdd;
     int counter = 0;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view);
 
         //Setting Listener
-        tvResult =(TextView)findViewById(R.id.tv_result);
+        tvResult = findViewById(R.id.tv_result);
+
+        long time =CurrentTimeMillisClock.getInstance().now();
+        String timeString = Long.toString(time);
+        Log.i("test","class_timeStamp:"+timeString);
+
+//        //原始版systme，內建的，只是效率稍微慢些，但是不用創CurrentTimeMillisClock類別
+//        long totalMilliSeconds = System.currentTimeMillis();
+//        //ms
+//        String info_t1m = Long.toString(totalMilliSeconds);
+//        Log.i("test","System:"+info_t1m);
 
     }
 
